034c74336d097ea42208dda415b26b8c071ae27a
kimjiwon <kimjiwon@localhost>
2022-09-29 14:27:38 +0900

    ..

--- support_fire/src/main/java/com/itwill/mapper/OrdersMapper.java
+++ support_fire/src/main/java/com/itwill/mapper/OrdersMapper.java
@@ -1,4 +1,43 @@
 package com.itwill.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.itwill.dto.Orders;
+
+@Mapper
 public interface OrdersMapper {
+	//고객한명구매내역전체보기
+	@Select("select * from orders o join productdetail pd on o.pd_no_purchase=pd.pd_no where m_id=#{m_id}")
+	public Orders selectByIdPurchase(String m_id);
+	
+	//고객한명판매내역전체보기
+	@Select("select * from orders o join productdetail pd on o.pd_no_sell=pd.pd_no where m_id=#{m_id}")
+	public Orders selectByIdSell(String m_id);
+	
+	//주문번호로주문한개조회
+	@Select("select * from orders where o_no=#{o_no}")
+	public Orders selectByNo(int o_no);
+	
+	//주문전체조회
+	@Select("select * from orders")
+	public List<Orders> selectAll();
+	
+	//주문번호한개삭제
+	@Delete("delete from orders where o_no=#{o_no}")
+	public int deleteByNo(int o_no);
+	
+	//주문정보한개업데이트(주문상태변경)
+	@Update("update orders set o_status=#{o_status} where o_no=#{o_no}")
+	public int updateByNo(Orders orders);
+	
+	//주문한개생성
+	@Insert("insert into orders (o_no, o_date, pd_no_purchase,pd_no_sell,o_status) values (orders_o_no_seq.nextval, TO_DATE(SYSDATE),#{pd_no_purchase},#{pd_no_sell},#{o_status})")
+	public int insertOrder(Orders orders);
+	
 }
--- support_fire/src/main/java/com/itwill/mapper/PaymentMapper.java
+++ support_fire/src/main/java/com/itwill/mapper/PaymentMapper.java
@@ -1,5 +1,18 @@
 package com.itwill.mapper;
 
-public interface PaymentMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.itwill.dto.Payment;
+
+@Mapper
+public interface PaymentMapper {
+	//
+	public Payment selectByPMNo(int pm_no);
+	public Payment selectByONo(int o_no);
+	public List<Payment> selecAll();
+	public int updateByNo(int o_no);
+	public int deleteByNo(int pm_no);
+	public int insertPayment(Payment payment);
 }
