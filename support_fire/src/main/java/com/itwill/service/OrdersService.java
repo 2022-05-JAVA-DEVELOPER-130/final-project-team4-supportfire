1b60de54b8428c75922cffbc21c6058c862c7a36
kimjiwon <kimjiwon@localhost>
2022-09-29 18:50:21 +0900

    ..

--- support_fire/src/main/java/com/itwill/service/OrdersService.java
+++ support_fire/src/main/java/com/itwill/service/OrdersService.java
@@ -1,5 +1,22 @@
 package com.itwill.service;
 
+import java.util.List;
+
+import com.itwill.dto.Orders;
+
 public interface OrdersService {
 
+	Orders selectByIdPurchase(String m_id) throws Exception;
+	
+	Orders selectByIdSell(String m_id) throws Exception;
+	
+	Orders selectByNo(int o_no) throws Exception;
+	
+	List<Orders> selectAll() throws Exception;
+	
+	int deleteByNo(int o_no) throws Exception;
+	
+	int updateByNo(Orders orders) throws Exception;
+	
+	int insertOrder(Orders orders) throws Exception;
 }
