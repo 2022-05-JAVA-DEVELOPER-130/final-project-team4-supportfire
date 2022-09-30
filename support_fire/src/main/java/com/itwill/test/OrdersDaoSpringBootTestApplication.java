86514d859b10cd54567ead100b6e217c60c0c7dc
kimjiwon <kimjiwon@localhost>
2022-09-30 13:41:35 +0900

    ..

--- support_fire/src/main/java/com/itwill/test/OrdersDaoSpringBootTestApplication.java
+++ support_fire/src/main/java/com/itwill/test/OrdersDaoSpringBootTestApplication.java
@@ -5,11 +5,18 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
 
 import com.itwill.dao.OrdersDao;
+import com.itwill.mapper.OrdersMapper;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.itwill"})
+@ComponentScan(basePackages = {"com.itwill"},includeFilters= {
+		@Filter(type=FilterType.ASSIGNABLE_TYPE,
+				classes = {OrdersDao.class,OrdersMapper.class}
+				)
+})
 public class OrdersDaoSpringBootTestApplication {
 
 	public static void main(String[] args) throws Exception {
@@ -19,9 +26,9 @@ public class OrdersDaoSpringBootTestApplication {
 		application.setWebApplicationType(WebApplicationType.NONE);
 		ConfigurableApplicationContext context=application.run(args);
 		OrdersDao ordersDao=(OrdersDao)context.getBean(OrdersDao.class);
-		//System.out.println(ordersDao.selectAll());
-		//System.out.println(ordersDao.selectByIdPurchase("jiwon"));
-		//System.out.println(ordersDao.selectByIdSell("jiwon"));
+		System.out.println(ordersDao.selectAll());
+		System.out.println(ordersDao.selectByIdPurchase("jiwon"));
+		System.out.println(ordersDao.selectByIdSell("jiwon"));
 		
 	}
 
