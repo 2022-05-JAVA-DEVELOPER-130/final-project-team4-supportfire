6f6d2b2864fbf248f4157e204fb7741dc3da3419
seongmin <posteto@naver.com>
2022-09-30 17:52:31 +0900

    ..

--- support_fire/src/main/java/com/itwill/test/ProductDetailDaoSpringBootTestApplication.java
+++ support_fire/src/main/java/com/itwill/test/ProductDetailDaoSpringBootTestApplication.java
@@ -12,24 +12,26 @@ import org.springframework.context.annotation.FilterType;
 import com.itwill.dao.CategoryDao;
 import com.itwill.dao.CategoryDaoImp;
 import com.itwill.dao.OrdersDao;
+import com.itwill.dao.ProductDetailDao;
 import com.itwill.mapper.CategoryMapper;
 import com.itwill.mapper.OrdersMapper;
+import com.itwill.mapper.ProductDetailMapper;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.itwill"},includeFilters= {
 		@Filter(type=FilterType.ASSIGNABLE_TYPE,
-				classes = {CategoryDao.class,CategoryMapper.class}
+				classes = {ProductDetailDao.class,ProductDetailMapper.class}
 				)
 })
-public class CategoryDaoSpringBootTestApplication {
+public class ProductDetailDaoSpringBootTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication application = 
-				new SpringApplication(CategoryDaoSpringBootTestApplication.class);
+				new SpringApplication(ProductDetailDaoSpringBootTestApplication.class);
 		application.setWebApplicationType(WebApplicationType.NONE);
 		ConfigurableApplicationContext context=application.run(args);
-		CategoryDao carCategoryDao=(CategoryDao)context.getBean(CategoryDao.class);
-		System.out.println(carCategoryDao.selectAll());
+		ProductDetailDao productDetailDao=(ProductDetailDao)context.getBean(ProductDetailDao.class);
+		System.out.println(productDetailDao.selectAll());
 
 	}
 
