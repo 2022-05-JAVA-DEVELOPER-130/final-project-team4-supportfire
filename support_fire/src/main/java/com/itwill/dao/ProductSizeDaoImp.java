8dfd705ea30b1ba0e2f1f31cb9509412bd00cbd6
김봉균 <@DESKTOP-3O2P0E9>
2022-10-04 00:17:55 +0900

    ..

--- support_fire/src/main/java/com/itwill/dao/ProductSizeDaoImp.java
+++ support_fire/src/main/java/com/itwill/dao/ProductSizeDaoImp.java
@@ -1,5 +1,31 @@
 package com.itwill.dao;
 
-public class ProductSizeDaoImp {
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.dto.ProductSize;
+import com.itwill.mapper.ProductSizeMapper;
+@Repository
+public class ProductSizeDaoImp implements ProductSizeDao{
+	@Autowired
+	private ProductSizeMapper productSizeMapper;
+	
+	@Override
+	public int delete(int ps_no) {
+		return productSizeMapper.delete(ps_no);
+	}
+	@Override
+	public ProductSize selectByNo(int ps_no) {
+		return productSizeMapper.selectByNo(ps_no);
+	}
+	@Override
+	public Map selectBuyMinPriceBySize(int p_no, String s_size) {
+		return productSizeMapper.selectBuyMinPriceBySize(p_no, s_size);
+	}
+	@Override
+	public Map selectSellMinPriceBySize(int p_no, String s_size) {
+		return productSizeMapper.selectSellMinPriceBySize(p_no, s_size);
+	}
 }
--- support_fire/src/main/java/com/itwill/mapper/ProductSizeMapper.java
+++ support_fire/src/main/java/com/itwill/mapper/ProductSizeMapper.java
@@ -1,6 +1,34 @@
 package com.itwill.mapper;
 
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+
+import com.itwill.dto.ProductSize;
+
+@Mapper
 public interface ProductSizeMapper {
+	//ps_no로 제품 삭제
+	@Delete("delete from productsize where ps_no=#{ps_no}")
+	int delete(int ps_size);
 	
+	//ps_no로 제품 1개 출력
+	@Select("select * from productSize ps join product p on ps.p_no = p.p_no where ps.ps_no = #{ps_no}")
+	@ResultMap("productSize")
+	ProductSize selectByNo(int ps_no);
 	
+	//제품 사이즈 구매 최소 가격 출력
+	@Select("select p.p_no, ps.s_size, min(pd.pd_price) from productsize ps join (select * from productdetail where bt_no = 1 and b_no = 1) pd on ps.ps_no = pd.ps_no join product p on ps.p_no = p.p_no where (p.p_no=#{p_no} and ps.s_size=#{s_size}) group by p.p_no, ps.s_size")
+	@ResultMap("selectMap")
+	Map selectBuyMinPriceBySize(int p_no, String s_size);
+	
+	//제품 사이즈 판매 최소 가격 출력
+	@Select("select p.p_no, ps.s_size, min(pd.pd_price) from productsize ps join (select * from productdetail where bt_no = 2 and b_no = 1) pd on ps.ps_no = pd.ps_no join product p on ps.p_no = p.p_no where ps.s_size=#{s_size} group by p.p_no, ps.s_size")
+	@ResultMap("selectMap")
+	Map selectSellMinPriceBySize(int p_no, String s_size);
+	
+	//제품 
 }
