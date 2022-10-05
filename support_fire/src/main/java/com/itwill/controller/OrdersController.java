7e33d3254e5d00be70006a52388ff00b1698fd29
kimjiwon <kimjiwon@localhost>
2022-10-05 16:44:22 +0900

    ..

--- support_fire/src/main/java/com/itwill/controller/OrdersController.java
+++ support_fire/src/main/java/com/itwill/controller/OrdersController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.dto.Member;
@@ -19,12 +20,33 @@ import com.itwill.dto.Orders;
 import com.itwill.service.OrdersService;
 
 
-@Controller
+@RestController
 public class OrdersController {
 	
 	@Autowired
 	private OrdersService ordersService;
-	
+
+	@RequestMapping(value="/rest_orders_purchase_list",params = "m_id")
+	public Map orders_purchase_list(@RequestParam String m_id,Model model ) throws Exception{
+		int code=0;
+		String url="";
+		String msg="";
+		List<Orders> data = new ArrayList<Orders>();
+		
+		List<Orders> ordersList= ordersService.selectByIdPurchase(m_id);
+		data.addAll(ordersList);
+		code=1;
+		msg="성공";
+	    url="orders_purchase_list";
+	    
+	    Map resultMap= new HashMap();
+	    resultMap.put("code",code);
+	    resultMap.put("url",url);
+	    resultMap.put("msg",msg);
+	    resultMap.put("data",data);
+	    
+	    return resultMap;
+	}
 	
 
 }
