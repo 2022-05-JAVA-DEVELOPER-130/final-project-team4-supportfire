b434a3a4933cd4b6b82b09c95bf95d4ea2d7c703
bongkyoun <kbk6706@gmail.com>
2022-10-11 13:39:35 +0900

    ..

--- support_fire/src/main/java/com/itwill/controller/ProductController.java
+++ support_fire/src/main/java/com/itwill/controller/ProductController.java
@@ -1,13 +1,25 @@
 package com.itwill.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.itwill.dto.Product;
 import com.itwill.service.ProductService;
 
 @Controller
 public class ProductController {
-
+	@Autowired
 	private ProductService productService;
+	
+	@RequestMapping(value = "test ")
+	@ResponseBody
+	public Product test() {
+		Product product = productService.selectByNo(1);
+		System.out.println("product : " + product);
+		return product;
+	}
 }
 
 
