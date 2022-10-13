d31f7bb46a160ff5b18288d77294037d26b986e6
bongkyoun <kbk6706@gmail.com>
2022-10-13 13:15:21 +0900

    ..

--- support_fire/src/main/java/com/itwill/controller/ProductSizeController.java
+++ support_fire/src/main/java/com/itwill/controller/ProductSizeController.java
@@ -1,9 +1,10 @@
 package com.itwill.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.service.ProductSizeService;
-
+@RestController
 public class ProductSizeController {
 	@Autowired
 	private ProductSizeService productSizeService;
